2abd53d73eb7ca7d74061fe1a911556bef508bc0
yeomsungpil <present7256@gmail.com>
2023-08-23 16:02:47 +0900

    mapView에 region과 annotation 설정 완료

--- CLLocation+Mapkit_Example/ViewController.swift
+++ CLLocation+Mapkit_Example/ViewController.swift
@@ -7,19 +7,63 @@
 
 import UIKit
 import CoreLocation
+import MapKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     let locationManager = CLLocationManager()
+    let mapView = MKMapView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         locationManager.delegate = self
+        // View에 mapView 올리기
+        view.addSubview(mapView)
+        view.backgroundColor = .white
+        // mapView 레이아웃 설정
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
        
         checkDeviceLocationAuthorization()
     }
     
+    // 내가 위치한 지역과 핀(Annotation) 설정
+    func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
+        // 디바이스 현재 위도 경도 CLLocationCoordinate2D(latitude: 37.517926, longitude: 126.886371)
+        
+        
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 100, longitudinalMeters: 100)
+        // mapView에 보여주기
+        mapView.setRegion(region, animated: true)
+        
+        // 현재 위치 핀 찍기
+        let annotation = MKPointAnnotation()
+        annotation.title = "임시 거점"
+        annotation.coordinate = center
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+    // 권한 - 허용안함을 눌렀을때 Alert 띄우고 iOS 설정 화면으로 이동
+    func showLocationSettingAlert() {
+        let alert = UIAlertController(title: "위치 정보 설정", message: "설정>개인 정보 보호 > 위치 여기로 이동해서 당장 진행시켜", preferredStyle: .alert)
+        let goSetting = UIAlertAction(title: "위치 설정하기", style: .default) { _ in
+            // iOS 설정 페이지로 이동 : openSettingsURLString
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
+            }
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(goSetting)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+    
+    // 상태가 바뀔때마다 권한 확인
     func checkDeviceLocationAuthorization() {
         
         DispatchQueue.global().async {
@@ -55,6 +99,7 @@ class ViewController: UIViewController {
             print("애기들 권한 막아놈")
         case .denied:
             print("권한 거부")
+            showLocationSettingAlert()
         case .authorizedAlways:
             print("항상 권한 허용")
         case .authorizedWhenInUse:
@@ -75,6 +120,9 @@ extension ViewController: CLLocationManagerDelegate {
     // 성공했을 경우
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print("위치 권한을 성공적으로 받아왔을때",locations)
+        if let coordinate = locations.last?.coordinate {
+            setRegionAndAnnotation(center: coordinate)
+        }
         
     }
     // 실패했을 경우
